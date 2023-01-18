12208e367baf5cd41bac96445c9d397fef3f54a0
Nahom <112133958+Nahomatnafu@users.noreply.github.com>
2023-01-18 12:37:36 -0600

    initial commit

---
+++ breadshop/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace breadshop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string buyer;
+            Console.WriteLine("Bread is ready.");
+            Console.WriteLine("Who is the bread for?");
+            buyer = Console.ReadLine();
+            Console.WriteLine($"Noted: {buyer} got the bread.");
+            Console.ReadLine();
+        }
+    }
+}
